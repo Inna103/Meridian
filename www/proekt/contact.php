6cdea8192fa8fa69042c113afdabc4856ff4f220
marinabosovik <bosovik94@mail.ru>
2015-11-28 23:08:12 +0200

    Стринадко: Капча, контакти

--- www/proekt/contact.php
+++ www/proekt/contact.php
@@ -30,7 +30,7 @@
 				<a href="biography.php" class='words_menu'>Про нас</a>
 				<a href="" class='words_menu'>Відгуки </a>
 				<a href="contact.php" class='words_menu'>Контакти</a>
-				<a href="user.php" class='words_menu'>Користувачу</a>
+				<a href="" class='words_menu'>Користувачу</a>
 			</ul>
 		</div>
 	
@@ -43,7 +43,7 @@
   <div class="left_details">
 			<h1>Туристична компанія </br> "Meridian" </h1>
 			<p> 
-              <span class="contact_title">Робочий телефон:</span> <span class="contact_detail">937-99-92</span> <br />
+       <span class="contact_title">Робочий телефон:</span> <span class="contact_detail">937-99-92</span> <br />
               <span class="contact_title">Мобільний телефон:</span> <span class="contact_detail">61 75555 5555</span> <br />
               <span class="blue"><span class="contact_title" >Адреса:</span></span><span class="contact_detail"> м.Чернівці<br /> вул. Університетська, 1 </span></p> <br />
 			  
@@ -55,21 +55,34 @@
     </div><!--end left-->
     
     <div class="right_details">
-          <p>Для того, щоб надіслати нам повідомлення на електронну пошту, заповніть форму нижче.</p>
-          <form action="formsubmit.php" method="post">
-            <fieldset id="contact">
-            <label for="your_name">Ім'я:</label>
-            <input name="your_name" type="text" class="forms" id="your_name" size="35" />
-            <label for="your_email">Email:</label>
-            <input name="your_email" type="text" class="forms" id="your_email" size="35" />
-            <label for="phone">Телефон:</label>
-            <input name="phone" type="text" class="forms" id="phone" size="35" />
-            <label for="message">Текст листа:</label>
-            <textarea name="message" cols="30" rows="6" class="forms" id="message"></textarea>
-			<input name="submit_button" type="image" class="submit_button" id="submit_button" title="Submit Message" src="images/button_submit2.gif" />
-
-            </fieldset>
-          </form>
+          <form action="send.php" method="post">             
+				<p>Ім'я
+				<input name="name" size="50" type="text">
+				</p>
+ 
+              <p>Email 
+			<input name="mail" size="50" type="text">
+				</p>
+				
+              <p>Повідомлення
+ 
+   
+ 
+              <textarea cols="50" rows="9" name="text"></textarea>
+				
+ <p>
+ Введіть текст зображення:
+<br/>
+ <img style="border: 1px solid gray; background: url('bg_capcha.png');" src = "captcha.php" width="120" height="40"/>
+<br/>
+<form action="" method="POST">
+	 <input type="text" name="capcha" />
+	 <p><input type="submit" value="Надіслати" name="button"></p>
+</form> 
+				</p>
+   
+   </p>
+</form>
         
     
     
---
+++ www/proekt/send.php
@@ -0,0 +1,40 @@
+﻿<strong><?
+$send = $_POST['button'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$text = $_POST['text'];
+if ($send != "button"){
+ 
+$to = "rapemi77@mail.ru";                 // вставте свой емаил
+$subject = "Письмо с сайта YourSite.ru";        
+$mailheaders .="Reply-To: $mailnn";
+ 
+$msg .= "Имя отправителя:ntt$namen";
+$msg .= "E-mail отправителя:ntt$mailn";
+$msg .= "Сообщение: n$textnnn";
+ session_start();
+ if($_POST['capcha'] != $_SESSION['capcha'])
+ {echo "<center>Текст із зображення введено не вірно!</center>";
+	echo '<center>Через кілька секунд ви будете перенаправлені на попередню сторінку для повторного введення даних.</center>';
+
+ header('Refresh: 5; URL=/contact.php');}
+else{
+ if(!empty($name) && !empty($mail) && !empty($text)) {
+        
+mail ($to, $subject, $msg, $mailheaders);
+ 
+    $mess= "Шановний $name, ваше повідомлення було успішно надіслане."; 
+ 
+ echo "<center>$mess</center> ";
+
+header('Refresh: 3; URL=/index.php'); 
+echo '<center>Через кілька секунд ви будете перенаправлені на головну сторінку.</center>'; 
+exit; 
+    } else { 
+	header('Refresh: 5; URL=/contact.php');
+    echo '<center>Дані не введено.</center>'; 
+	echo '<center>Через кілька секунд ви будете перенаправлені на попередню сторінку для коригування даних.</center>'; 
+}
+}
+}
+?></strong>
