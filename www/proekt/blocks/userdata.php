ff6fde3d55baae142fc78ca6954105a13af464e9
marinabosovik <bosovik94@mail.ru>
2015-12-01 00:09:33 +0200

    Стринадко: привітання із авторизованим користувачем

--- www/proekt/blocks/userdata.php
+++ www/proekt/blocks/userdata.php
@@ -1,4 +1,10 @@
 <?php session_start();?>
 <form action="auth.php" method="POST">
-		<input class="button" type="submit" name="logout" value="Вихід" required>
+<?if(isset($_SESSION['login']))
+{ $name = $_SESSION['login'];
+$mess= "Привіт, $name";
+echo "<center>$mess</center> ";
+	}
+?>
+		 <input class="button" type="submit" name="logout" value="Вихід" required>
 </form>
